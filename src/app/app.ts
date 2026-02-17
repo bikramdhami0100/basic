97e9114f256a7f1cb2f2de5578edea344ebd9049
bikramdhami0100 <bikramdhami334@gmail.com>
2026-02-17 11:44:59 +0545

    data transfer from child to parent and parent to child component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,45 +5,17 @@ import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Child],
+  imports: [RouterOutlet, Child],
+  // templateUrl: './app.html',
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  users=signal(['bikram','sarita','sunita']);
-  // protected readonly title = signal('Basic to advanced Angular');
-//  count=signal(10);
-//  color="red";
-//  fruits=signal("apple");
-//    constructor(){
-//      effect(()=>{
-//       // console.log('color ',this.color);
-//       console.log("count: ",this.count());
-//        if(this.count()>50 && this.count()<100){
-//          this.color="green"
-//        }else if (this.count()>100&& this.count()<150){
-//          this.color="blue"
-//        }else if(this.count()>150){ 
-//          this.color="black"
-//        }
-//      })
-
-//      effect(()=>{
-//         console.log("color : ",this.fruits());
-//      });
-    
-     
-//    }
-//   updateCount(){
-//     // this.count="blue";
-//     this.count.set(this.count()+10);
-//   }
-//    updateChangeColor(){
-//         this.color="gray";
-//      }
-//      changeFruit(){
-//         this.fruits.set("banana");
-//      }
-  
-
+ users=signal(["bikram","sarita","ram"]);
+ selectedUser=signal("");
+ 
+ handleDelete(user:string){
+  console.log(user);
+  this.users.update((item)=>item.filter((item)=>item!=user))
+ }
 }
--- src/app/child/child.ts
+++ src/app/child/child.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,5 +7,23 @@ import { Component, Input } from '@angular/core';
   styleUrl: './child.css',
 })
 export class Child {
-  @Input() userName: string|undefined;
-}
+ @Input() userlists:any|string[] = [];
+@Output() selectedUser = new EventEmitter<string>();
+@Output() userDelete = new EventEmitter<string>();
+  handlerUser(){
+    console.log(this.userlists);
+  }
+  
+  handleSelectUser(user:string){
+    // console.log(this.selectedUser);
+    this.selectedUser.emit(user);
+
+  }
+  handleDelete(user:string){
+    // console.log(this.deleteUser);
+
+    this.userDelete.emit(user);
+  }
+  
+  
+} 
