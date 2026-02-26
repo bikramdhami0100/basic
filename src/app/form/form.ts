b40d256e3d228fb22e4725b4ebbf23615435ef57
bikramdhami0100 <bikramdhami334@gmail.com>
2026-02-26 12:09:46 +0545

    validatiaon in form

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   imports: [ReactiveFormsModule],
@@ -9,11 +9,21 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class Form {
   
   loginForm=new FormGroup({
-    name:new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl('')
-  })
+    name:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+  });
+  get name(){return this.loginForm.get('name')
+  }
+
+  get email (){return this.loginForm.get('email')
+  }
+
+  get password(){return this.loginForm.get('password')
+  }
+
   handleForm(){
+    console.log(this.loginForm.errors)
     console.log(this.loginForm.value);
   }
   resetData(){
